98ac5854e79e35aff88a01017e303e5dc157d0b2
fiifiGh <francisk994@gmail.com>
2025-06-19 09:24:08 -0500

    model updated

--- api/models.py
+++ api/models.py
@@ -12,19 +12,26 @@ class Category(models.Model):
 
 class Todo(models.Model):
     
-    STATUS_TYPES = {
-        'pending':'PENDING',
-        'in progress': 'IN PROGRESS',
-        'completed ':'COMPLETED'
-        }
+    class StatusTypes(models.TextChoices):
+        PENDING = "Pending"
+        IN_PROGRESS = "In Progress"
+        COMPLETED = "Completed"
     
     
     title = models.CharField(max_length=50)
     description = models.TextField()
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    completed_at = models.DateTimeField()
-    status = models.CharField(choices=STATUS_TYPES)
+    completed_at = models.DateTimeField(blank=True)
+    status = models.CharField(choices=StatusTypes.choices, default= StatusTypes.PENDING)
+    
+    
+    @property
+    def duration(self):
+        date = self.completed_at - self.created_at
+        return date.total_seconds
+        
+        
     
     def __str__(self):
         return self.title
