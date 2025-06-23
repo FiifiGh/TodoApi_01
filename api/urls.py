5ea5eb6bccd48f959d182b30c9f3fc228f09d765
fiifiGh <francisk994@gmail.com>
2025-06-22 19:50:43 -0500

    add filter functionality

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import category_list, category_add, todo_add
+from .views import category_list, category_add, todo, todo_update
 
 urlpatterns = [
     path('categories/', category_list),
     path('categories/add/', category_add),
-    path('todo/', todo_add)
+    path('todo/', todo),
+    path('todo/<int:pk>', todo_update)
 ]
--- api/views.py
+++ api/views.py
@@ -2,8 +2,8 @@ from .serializers import CategorySerializer, TodoSerializer
 from .models import Category, Todo
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import status
-
+from rest_framework import status as http_status
+ 
 # Create your views here.
 
 @api_view(['GET'])
@@ -30,8 +30,8 @@ def category_add(request):
             'message': str(e)
         }, status=status.HTTP_400_BAD_REQUEST)
     
-@api_view(['POST', 'GET'])
-def todo_add(request):
+@api_view(['POST', 'GET',])
+def todo(request):
     
     if (request.method == 'POST'):
         serialize_data = TodoSerializer(data = request.data)
@@ -48,12 +48,60 @@ def todo_add(request):
                 'message': str(e)
             }, status=status.HTTP_400_BAD_REQUEST)
     elif (request.method == 'GET'):
-        data = Todo.objects.all()
-        serialize_data = TodoSerializer(data, many=True)
-        return Response({
-            'data': serialize_data.data
-        })
-        
-    
+        status = request.query_params.get('status')
+        category_id = request.query_params.get('category')
+        if (status != None and category_id != None):
+            status = str(status).capitalize()
+            id = int(category_id)
+            results =  Todo.objects.filter(status=status, category__id=id)
+            serialize_result =  TodoSerializer(results, many = True)
+            return Response(
+                {
+                    'result' : serialize_result.data,
+                    'status' : 'success' 
+                },
+                status= http_status.HTTP_200_OK
+            )
+        elif status != None:
+            status = str(status).capitalize()
+            results =  Todo.objects.filter(status=status)
+            serialize_result =  TodoSerializer(results, many = True)
+            return Response(
+                    {
+                        'result' : serialize_result.data,
+                        'status' : 'success' 
+                    },
+                    status= http_status.HTTP_200_OK
+                )
+        elif category_id != None:
+            category_id = int(category_id)
+            results =  Todo.objects.filter(category__id=category_id)
+            serialize_result =  TodoSerializer(results, many = True)
+            return Response(
+                    {
+                        'result' : serialize_result.data,
+                        'status' : 'success' 
+                    },
+                    status= http_status.HTTP_200_OK
+                )
+             
+    data = Todo.objects.all()
+    serialize_data = TodoSerializer(data, many=True)
+    return Response({'data': serialize_data.data})  
 
+
+@api_view(['PUT'])
+def todo_update(request, pk):
+    try:
+        task = Todo.objects.get(id=pk)
+        task.status = Todo.StatusTypes.COMPLETED
+        task.save()
+        return Response({'message': 'success'}, status=status.HTTP_200_OK)
+    except task.DoesNotExist:
+        return Response(
+                {'message': 'Todo not found'},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+    except Exception as e:
+        return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             
